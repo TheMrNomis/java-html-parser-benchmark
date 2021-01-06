7ea104549ece1f133abf052f74d98551e73c50b8
git <git@homnomnom.fr>
2021-01-06 10:25:02 +0100

    first commit

---
+++ src/main/java/com/homnomnom/fr/App.java
@@ -0,0 +1,13 @@
+package com.homnomnom.fr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
